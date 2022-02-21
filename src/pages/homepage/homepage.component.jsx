fdfe1259bf806895104c72caf31ce57e6f155689
N Aiuto <nickaiuto@gmail.com>
2022-02-21 14:45:39 -0600

    updating app with shop pate, preview and collection item

--- src/pages/homepage/homepage.component.jsx
+++ src/pages/homepage/homepage.component.jsx
@@ -6,12 +6,14 @@ import './homepage.styles.scss';
 
 import Directory from '../../components/directory/directory.component';
 
-const HomePage = ({history}) => (
-    <div className='homepage'>
-        <Link to='/hats'>HATS</Link>
-
-        <Directory/>
-    </div >
-)
+const HomePage = ({ ...ri }) => {
+    // ri = router items (history, location, match) passed in as props from Route component={HomePage}
+    console.log(ri);
+    return (
+        <div className='homepage'>
+            <Directory />
+        </div >
+    );
+}
 
 export default HomePage;

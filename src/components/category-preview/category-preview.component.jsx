9e73b0ebe5778c6f8e9b92fddebabe0f63d6e64e
Nicky Aiuto <nickaiuto@gmail.com>
2022-03-15 16:43:05 -0500

    update to use styled components; home page nav

--- src/components/category-preview/category-preview.component.jsx
+++ src/components/category-preview/category-preview.component.jsx
@@ -1,27 +1,26 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import ProductCard from '../product-card/product-card.component';
 
-import ProductCard  from '../product-card/product-card.component';
+import {
+  CategoryPreviewContainer,
+  Title,
+  Preview,
+} from './category-preview.styles';
 
-import './category-preview.styles.scss';
-
-const CategoryPreview = ( {title, products} ) => {
+const CategoryPreview = ({ title, products }) => {
   return (
-    <div className='category-preview-container'>
+    <CategoryPreviewContainer>
       <h2>
-        <Link to={title}>{title.toUpperCase()}</Link>
+        <Title to={title}>{title.toUpperCase()}</Title>
       </h2>
-      <div className='preview'>
-        {
-          products
-            .filter((_, idx) => idx < 4)
-            .map((product) => (              
-              <ProductCard key={product.id} product={product} />
-            ))
-        }
-      </div>
-    </div>
+      <Preview>
+        {products
+          .filter((_, idx) => idx < 4)
+          .map((product) => (
+            <ProductCard key={product.id} product={product} />
+          ))}
+      </Preview>
+    </CategoryPreviewContainer>
   );
-}
+};
 
 export default CategoryPreview;
--- src/routes/shop/shop.component.jsx
+++ src/routes/shop/shop.component.jsx
@@ -1,7 +1,5 @@
 import { Routes, Route } from 'react-router-dom';
 
-import './shop.styles.scss';
-
 import CategoriesPreview from '../categories-preview/categories-preview.component';
 import Category from '../category/category.component';
 
@@ -9,9 +7,9 @@ const Shop = () => {
   return (
     <Routes>
       <Route index element={<CategoriesPreview />} />
-      <Route path=":category" element={<Category />} />
+      <Route path=':category' element={<Category />} />
     </Routes>
   );
-};                                   
+};
 
 export default Shop;

15334f6ca8d2526c0710c32f5738f66c6a5840ad
Nicky Aiuto <nickaiuto@gmail.com>
2022-03-16 14:18:15 -0500

    add stripe payments

--- src/routes/checkout/checkout.component.jsx
+++ src/routes/checkout/checkout.component.jsx
@@ -3,12 +3,14 @@ import { useContext } from 'react';
 import { CartContext } from '../../contexts/cart.context';
 
 import CheckoutItem from '../../components/checkout-item/checkout-item.component';
+import StripeCheckoutButton from '../../components/stripe-button/stripe-button.component';
 
 import {
   CheckoutContainer,
   CheckoutHeader,
   HeaderBlock,
   Total,
+  TestWarning,
 } from './checkout.styles';
 
 const Checkout = () => {
@@ -37,6 +39,13 @@ const Checkout = () => {
         <CheckoutItem key={cartItem.id} cartItem={cartItem} />
       ))}
       <Total>Total: ${cartTotal}</Total>
+      <TestWarning>
+        *Please use the following test credit card information*
+        <br/>
+        4242 4242 4242 4242 - Exp (anything in the future) - CVV - any three digits
+      </TestWarning>
+      <StripeCheckoutButton price={cartTotal} />
+
     </CheckoutContainer>
   );
 };
